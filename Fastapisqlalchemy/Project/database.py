a592125d4b1fbb8ede38fa7fde62b025b6f1acb3
rajbordiya31 <raj.bordiya@gammaedge.io>
2025-08-07 11:07:20 +0530

    Modified fastapisqlalchemy added

--- Fastapisqlalchemy/Project/database.py
+++ Fastapisqlalchemy/Project/database.py
@@ -8,3 +8,7 @@ engine = create_engine(Database_url,pool_pre_ping=True)
 Sessionlocal = sessionmaker(bind=engine,autocommit =False,autoflush=False)
 
 Base= declarative_base()
+
+# engine =create_engine("mysql+mysqldb://root:Root%40123@localhost/practice")
+# connection = engine.connect()
+# print("connected")
--- Fastapisqlalchemy/Project/models.py
+++ Fastapisqlalchemy/Project/models.py
@@ -1,5 +1,6 @@
 from database import Base
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,ForeignKey
+from sqlalchemy.orm import relationship
 
 class Student(Base):
     __tablename__ = "studentinfo"
@@ -9,4 +10,15 @@ class Student(Base):
     age=Column(Integer)
     mob = Column(String(100))
 
+    user = relationship("User",back_populates="student")
+
+# class User(Base):
+#     __tablename__ = "user"
+    
+#     id = Column(Integer,primary_key=True,index=True)
+#     username =Column(String(100),index=True,unique=True)
+#     hash_password = Column(String)
+#     Student_id = Column(Integer,ForeignKey("studentinfo.id"))
+#     student = relationship("Student",back_populates="user")
+    
 
